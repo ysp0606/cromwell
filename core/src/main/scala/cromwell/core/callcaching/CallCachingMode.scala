273af4ef8db3b7e4e65bd5f15b9d3d82c5c0860d
Miguel Covarrubias <mcovarr@broadinstitute.org>
2017-05-17 17:06:14 -0400

    Revert "cleanup"
    This reverts commit ab97638715adba9ae699a1fa367992274ab6fd85.

--- core/src/main/scala/cromwell/core/callcaching/CallCachingMode.scala
+++ core/src/main/scala/cromwell/core/callcaching/CallCachingMode.scala
@@ -36,5 +36,3 @@ case object WriteCache extends ReadWriteMode { override val r = false }
 case object ReadAndWriteCache extends ReadWriteMode
 
 final case class CallCachingOptions(invalidateBadCacheResults: Boolean = true)
-
-case class DockerWithHash(dockerAttribute: String)
