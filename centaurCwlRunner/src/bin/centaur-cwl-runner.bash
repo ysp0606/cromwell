a4852de1d04e3bcc632fbf45b27cb7965d045b38
Dan Billings <danb@broadinstitute.org>
2017-12-08 10:18:29 -0500

    ruchi was here

--- centaurCwlRunner/src/bin/centaur-cwl-runner.bash
+++ centaurCwlRunner/src/bin/centaur-cwl-runner.bash
@@ -3,4 +3,5 @@
 # `sbt assembly` must have already been run.
 CENTAUR_CWL_JAR="${CENTAUR_CWL_JAR:-"$( find "$( dirname "${BASH_SOURCE[0]}" )/../../target/scala-2.12" -name 'centaur-cwl-runner-*.jar' )"}"
 
-java -jar "${CENTAUR_CWL_JAR}" $@
+#java -jar "${CENTAUR_CWL_JAR}" $@
+java -jar "/Users/danb/cromwell/centaurCwlRunner/target/scala-2.12/centaur-cwl-runner-31-d32d789-SNAP.jar" $@
