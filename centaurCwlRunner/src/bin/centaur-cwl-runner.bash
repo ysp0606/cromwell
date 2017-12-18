e83fcade5829da40894bccd2eaa1b63aa5659bc2
Miguel Covarrubias <mcovarr@broadinstitute.org>
2017-12-18 17:30:44 -0500

    PAPI CWL conformance.

--- centaurCwlRunner/src/bin/centaur-cwl-runner.bash
+++ centaurCwlRunner/src/bin/centaur-cwl-runner.bash
@@ -2,5 +2,18 @@
 
 # `sbt assembly` must have already been run.
 CENTAUR_CWL_JAR="${CENTAUR_CWL_JAR:-"$( find "$( dirname "${BASH_SOURCE[0]}" )/../../target/scala-2.12" -name 'centaur-cwl-runner-*.jar' )"}"
+SERVICE_ACCOUNT_JSON="$( dirname "${BASH_SOURCE[0]}" )/../../../cromwell-service-account.json"
 
-java -jar ${CENTAUR_CWL_JAR} $@
+if [ -e "${SERVICE_ACCOUNT_JSON}" ]
+then
+  MODE="--mode-gcs-service=${SERVICE_ACCOUNT_JSON}"
+else
+  MODE="--mode-local"
+fi
+
+# Allow setting "CENTAUR_CWL_RUNNER_MODE=--mode-gcs-default" for local GCS Application Default usage.
+CENTAUR_CWL_RUNNER_MODE=${CENTAUR_CWL_RUNNER_MODE:-$MODE}
+
+>&2 echo using CENTAUR_CWL_RUNNER_MODE="${CENTAUR_CWL_RUNNER_MODE}"
+
+java -jar "${CENTAUR_CWL_JAR}" "${CENTAUR_CWL_RUNNER_MODE}" "$@"
